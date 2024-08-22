1987ad7cd00b9b944969695ba143d65a893b8d69
Anna Kazak <anna.s.kazak@gmail.com>
2024-08-22 09:27:33 +0200

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let i = 0;
+while ( i <= 20) {
+    sum +=i;
+    i++;
+}
+document.write(sum)
