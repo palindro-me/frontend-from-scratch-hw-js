9f19ceace87a7f8bc0237fc1e4420f8806108134
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-03 12:38:38 +0200

    Even function ex is done

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(x) {
+    result = (x % 2 === 0) ? true : false;
+    return result;
+}
+
+
