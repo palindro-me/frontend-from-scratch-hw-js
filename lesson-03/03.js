d96e929f1871aebe5be7e7524f5df944c9512063
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-03 12:46:24 +0200

    find largest account is done

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,11 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a,b,c) {
+    if (a > b && a > c) {
+        return a;
+    } else if (b > a && b > c) {
+        return b;
+    } else {
+        return c
+    }
+}
