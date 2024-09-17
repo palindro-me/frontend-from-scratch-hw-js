f7f4090abc6cc3872b2b7e9e8eb6b093fa0bbbfc
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-17 17:49:49 +0200

    fixed returntypo

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -14,9 +14,11 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
 function includesElement(array, element) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] !== element) {
-            return false;
-        } return true;
+    for (i = 0; i < array.length; i++) {
+
+        if (array[i] === element) {
+            return true;
+        }
     }
+    return false
 }
