d595fd89c482a9ce9de7e4917b218db6eccd91e5
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-17 19:24:45 +0200

    ex 4.2 is done

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,15 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+
+let uniqueArray = [];
+
+for(i = 0; i < array.length; i++) {
+    if (!findUniqueElements(uniqueArray, array[i])) {
+        uniqueArray.push(array[i])
+    }
+}
+
+return uniqueArray
+}
