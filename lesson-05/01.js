e89e4ddd870aedd0eeea33877e2127325110bec4
Anna Kazak <anna.s.kazak@gmail.com>
2024-09-24 09:47:17 +0200

    medthod function

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    if(resource === 'gold') {
+      game.resources[gold] += amount;
+    } else if (resource === 'lumber') {
+     game.resources[lumber] += amount
+    }
+  }
 }
