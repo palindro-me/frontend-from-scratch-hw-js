1e22d95d6c5a2fc34deba49b672a8a10bc2c7444
Anna Kazak <anna.s.kazak@gmail.com>
2024-10-02 16:56:00 +0200

    fixed a typo in if condition

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -10,7 +10,7 @@ function capitalizeWords(str) {
   let result = '';
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === 0 || str[i - 1] === ' ') {
+    if (i === 0 || str[i - 1] === ' ') {
       result += str[i].toUpperCase()
     } else {
       result += str[i]
